5811e327bfc1ab01ebd6414e522177ea95a4aa39
Robby Herron <robbyherron@theluxergroup.com>
2022-07-22 10:52:16 -0400

    build code for next publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
 import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
@@ -23,6 +24,7 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
+      json(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
   },
